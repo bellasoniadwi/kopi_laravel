db0dbbf789fd64f9bf63b3b17bc199c86f3a1ca2
anjanidwilestari <ayianjani774@gmail.com>
2023-08-31 09:23:32 +0700

    pasang template map persebaran

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -7,171 +7,91 @@
 @section('dashboard', 'active bg-gradient-kopi')
 
 @section('content')
-{{-- @if (Route::has('login'))
-<div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
-    @auth
-        <a href="{{ url('/') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Home</a>
-    @else
-        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+<body class="g-sidenav-show  bg-gray-200">
+    <div class="container-fluid py-4">
+        <div class="row min-vh-80">
+            <div class="col-12">
+                <div class="card h-100">
+                <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+                    <div class="bg-gradient-kopi shadow-kopi border-radius-lg pt-4 pb-3">
+                    <h5 class="text-white text-capitalize ps-3">Persebaran Kopi</h5>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div id="vector-map" class="mt-5 min-height-500"></div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-        @if (Route::has('register'))
-            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
-        @endif
-    @endauth
-</div>
-@endif --}}
-    <div class="row">
-      <!-- export rekap data from firestore -->
-      <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
-        <a href="{{ route('export.kehadiran') }}" class="btn btn-success">Export Excel</a>
-      </div>
-      <!-- end rekap students data from firestore -->
-    </div>
-    <br>
-    <div class="row">
-      <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
-        <div class="card">
-            <div class="card-header p-3 pt-2">
-                <div
-                    class="icon icon-lg icon-shape bg-gradient-dark shadow-dark text-center border-radius-xl mt-n4 position-absolute">
-                    <i class="material-icons opacity-10">person</i>
-                </div>
-                <div class="text-end pt-1">
-                    <p class="text-sm mb-0 text-capitalize">Jumlah Pengguna</p>
-                    <h4 class="mb-0">{{ $totalStudentInAMonth }}</h4>
-                </div>
-            </div>
-            <hr class="dark horizontal my-0">
-            <div class="card-footer p-3">
-                <p class="mb-0">Tercatat <span class="text-success text-sm font-weight-bolder">{{ $totalStudentInAMonth }}</span> akun pengguna</p>
-            </div>
-        </div>
-    </div>
-        <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
-            <div class="card">
-                <div class="card-header p-3 pt-2">
-                    <div
-                        class="icon icon-lg icon-shape bg-gradient-info shadow-success text-center border-radius-xl mt-n4 position-absolute">
-                        <i class="material-icons opacity-10">person</i>
-                    </div>
-                    <div class="text-end pt-1">
-                        <p class="text-sm mb-0 text-capitalize">Jumlah Masuk</p>
-                        <h4 class="mb-0">{{ $totalMasuk }}</h4>
-                    </div>
-                </div>
-                <hr class="dark horizontal my-0">
-                <div class="card-footer p-3">
-                    <p class="mb-0">Tercatat <span class="text-success text-sm font-weight-bolder">{{ $totalMasuk }}</span> siswa bulan ini</p>
-                </div>
-            </div>
-            <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
-                <div class="card">
-                    
-                </div>
-            </div>
-        </div>
-        <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
-            <div class="card">
-                <div class="card-header p-3 pt-2">
-                    <div
-                        class="icon icon-lg icon-shape bg-gradient-warning shadow-warning text-center border-radius-xl mt-n4 position-absolute">
-                        <i class="material-icons opacity-10">person</i>
-                    </div>
-                    <div class="text-end pt-1">
-                        <p class="text-sm mb-0 text-capitalize">Jumlah Izin</p>
-                        <h4 class="mb-0">{{ $totalIzin }}</h4>
-                    </div>
-                </div>
-                <hr class="dark horizontal my-0">
-                <div class="card-footer p-3">
-                    <p class="mb-0">Tercatat <span class="text-success text-sm font-weight-bolder">{{ $totalIzin }}</span> siswa bulan ini</p>
-                </div>
-            </div>
-            <div class="col-xl-4 col-sm-6 mb-xl-0 mb-4">
-                <div class="card">
-                    
-                </div>
-            </div>
-        </div>
-        <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
-            <div class="card">
-                <div class="card-header p-3 pt-2">
-                    <div
-                        class="icon icon-lg icon-shape bg-gradient-primary shadow-primary text-center border-radius-xl mt-n4 position-absolute">
-                        <i class="material-icons opacity-10">person</i>
-                    </div>
-                    <div class="text-end pt-1">
-                        <p class="text-sm mb-0 text-capitalize">Jumlah Sakit</p>
-                        <h4 class="mb-0">{{ $totalSakit }}</h4>
-                    </div>
-                </div>
-                <hr class="dark horizontal my-0">
-                <div class="card-footer p-3">
-                    <p class="mb-0">Tercatat <span class="text-success text-sm font-weight-bolder">{{ $totalSakit }}</span> siswa bulan ini</p>
-                </div>
-            </div>
-            <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
-                <div class="card">
-                    
-                </div>
-            </div>
-        </div>
-    </div>
-    <br><br>
-    <div class="row">
-      <!-- export rekap data from firestore -->
-      <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
-        <a href="{{ route('export.rekap') }}" class="btn btn-success">Export Excel</a>
-      </div>
-      <!-- end rekap students data from firestore -->
-        <div class="col-12">
-          <div class="card my-4">
-            <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
-              <div class="bg-gradient-kopi shadow-kopi border-radius-lg pt-4 pb-3">
-                <h6 class="text-white text-capitalize ps-3">Rekap Kehadiran {{ $currentMonthYearNow}}</h6>
-              </div>
-            </div>
-            <div class="card-body px-0 pb-2">
-              <div class="table-responsive p-0">
-                <table class="table align-items-center mb-0">
-                  <thead>
-                    <tr>
-                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Nama</th>
-                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Jumlah Masuk</th>
-                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Jumlah Izin</th>
-                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Jumlah Sakit</th>
-                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Tanpa Keterangan</th>
-                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Predikat</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    @foreach($totals as $name => $total)
-                    <tr>
-                      <td class="align-middle text-center">
-                        <p class="text-xs font-weight-bold mb-0">{{ $name }}</p>
-                      </td>
-                      <td class="align-middle text-center">
-                        <p class="text-xs font-weight-bold mb-0">{{ $total['masuk'] }}</p>
-                      </td>
-                      <td class="align-middle text-center">
-                        <p class="text-xs font-weight-bold mb-0">{{ $total['izin'] }}</p>
-                      </td>
-                      <td class="align-middle text-center">
-                        <p class="text-xs font-weight-bold mb-0">{{ $total['sakit'] }}</p>
-                      </td>
-                      <td class="align-middle text-center">
-                        <p class="text-xs font-weight-bold mb-0">{{ $totalWithoutKeteranganPerName[$name] }}</p>
-                      </td>
-                      <td class="align-middle text-center">
-                        <p class="text-xs font-weight-bold mb-0">{{ $predikat }}</p>
-                      </td>
-                    </tr>
-                    @endforeach
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    <!--   Core JS Files   -->
+    <script src="../assets/js/core/popper.min.js"></script>
+    <script src="../assets/js/core/bootstrap.min.js"></script>
+    <script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
+    <script src="../assets/js/plugins/smooth-scrollbar.min.js"></script>
+    <script src="../assets/js/plugins/world.js"></script>
+    <script>
+      // Initialize the vector map
+      var map = new jsVectorMap({
+        selector: "#vector-map",
+        map: "world_merc",
+        zoomOnScroll: false,
+        zoomButtons: false,
+        selectedMarkers: [1, 3],
+        markersSelectable: true,
+        markers: [{
+            name: "USA",
+            coords: [40.71296415909766, -74.00437720027804]
+          },
+          {
+            name: "Germany",
+            coords: [51.17661451970939, 10.97947735117339]
+          },
+          {
+            name: "Brazil",
+            coords: [-7.596735421549542, -54.781694323779185]
+          },
+          {
+            name: "Russia",
+            coords: [62.318222797104276, 89.81564777631716]
+          },
+          {
+            name: "China",
+            coords: [22.320178999475512, 114.17161225541399],
+            style: {
+              fill: '#E91E63'
+            }
+          }
+        ],
+        markerStyle: {
+          initial: {
+            fill: "#e91e63"
+          },
+          hover: {
+            fill: "E91E63"
+          },
+          selected: {
+            fill: "E91E63"
+          }
+        },
+  
+  
+      });
+    </script>
+    <script>
+      var win = navigator.platform.indexOf('Win') > -1;
+      if (win && document.querySelector('#sidenav-scrollbar')) {
+        var options = {
+          damping: '0.5'
+        }
+        Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+      }
+    </script>
+    <!-- Github buttons -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+    <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
+    <script src="../assets/js/material-dashboard.min.js?v=3.1.0"></script>
+  </body>
+    
 @endsection
---
+++ resources/views/pages/dashboardd.blade.php
@@ -0,0 +1,120 @@
+@extends('newlayout.main')
+
+@section('title')
+    Dashboard
+@endsection
+
+@section('dashboard', 'active bg-gradient-kopi')
+
+@section('content')
+{{-- @if (Route::has('login'))
+<div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+    @auth
+        <a href="{{ url('/') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Home</a>
+    @else
+        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+
+        @if (Route::has('register'))
+            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+        @endif
+    @endauth
+</div>
+@endif --}}
+    <div class="row">
+      <!-- export rekap data from firestore -->
+      <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+        <a href="{{ route('export.kehadiran') }}" class="btn btn-success">Export Excel</a>
+      </div>
+      <!-- end rekap students data from firestore -->
+    </div>
+    <br>
+    <div class="row">
+      <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+        <div class="card">
+            <div class="card-header p-3 pt-2">
+                <div
+                    class="icon icon-lg icon-shape bg-gradient-dark shadow-dark text-center border-radius-xl mt-n4 position-absolute">
+                    <i class="material-icons opacity-10">person</i>
+                </div>
+                <div class="text-end pt-1">
+                    <p class="text-sm mb-0 text-capitalize">Jumlah Pengguna</p>
+                    <h4 class="mb-0">{{ $totalStudentInAMonth }}</h4>
+                </div>
+            </div>
+            <hr class="dark horizontal my-0">
+            <div class="card-footer p-3">
+                <p class="mb-0">Tercatat <span class="text-success text-sm font-weight-bolder">{{ $totalStudentInAMonth }}</span> akun pengguna</p>
+            </div>
+        </div>
+    </div>
+        <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+            <div class="card">
+                <div class="card-header p-3 pt-2">
+                    <div
+                        class="icon icon-lg icon-shape bg-gradient-info shadow-success text-center border-radius-xl mt-n4 position-absolute">
+                        <i class="material-icons opacity-10">person</i>
+                    </div>
+                    <div class="text-end pt-1">
+                        <p class="text-sm mb-0 text-capitalize">Jumlah Masuk</p>
+                        <h4 class="mb-0">{{ $totalMasuk }}</h4>
+                    </div>
+                </div>
+                <hr class="dark horizontal my-0">
+                <div class="card-footer p-3">
+                    <p class="mb-0">Tercatat <span class="text-success text-sm font-weight-bolder">{{ $totalMasuk }}</span> siswa bulan ini</p>
+                </div>
+            </div>
+            <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                <div class="card">
+                    
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+            <div class="card">
+                <div class="card-header p-3 pt-2">
+                    <div
+                        class="icon icon-lg icon-shape bg-gradient-warning shadow-warning text-center border-radius-xl mt-n4 position-absolute">
+                        <i class="material-icons opacity-10">person</i>
+                    </div>
+                    <div class="text-end pt-1">
+                        <p class="text-sm mb-0 text-capitalize">Jumlah Izin</p>
+                        <h4 class="mb-0">{{ $totalIzin }}</h4>
+                    </div>
+                </div>
+                <hr class="dark horizontal my-0">
+                <div class="card-footer p-3">
+                    <p class="mb-0">Tercatat <span class="text-success text-sm font-weight-bolder">{{ $totalIzin }}</span> siswa bulan ini</p>
+                </div>
+            </div>
+            <div class="col-xl-4 col-sm-6 mb-xl-0 mb-4">
+                <div class="card">
+                    
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+            <div class="card">
+                <div class="card-header p-3 pt-2">
+                    <div
+                        class="icon icon-lg icon-shape bg-gradient-primary shadow-primary text-center border-radius-xl mt-n4 position-absolute">
+                        <i class="material-icons opacity-10">person</i>
+                    </div>
+                    <div class="text-end pt-1">
+                        <p class="text-sm mb-0 text-capitalize">Jumlah Sakit</p>
+                        <h4 class="mb-0">{{ $totalSakit }}</h4>
+                    </div>
+                </div>
+                <hr class="dark horizontal my-0">
+                <div class="card-footer p-3">
+                    <p class="mb-0">Tercatat <span class="text-success text-sm font-weight-bolder">{{ $totalSakit }}</span> siswa bulan ini</p>
+                </div>
+            </div>
+            <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                <div class="card">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
